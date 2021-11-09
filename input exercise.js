b53cc07fedab06ded7774bf033c17b6d0e190393
duorfan <nf2111@nyu.edu>
2021-11-09 16:31:15 -0500

    loader & submit
    can't find a logical way of coding this

--- input exercise.js
+++ input exercise.js
@@ -1,14 +1,33 @@
+// const button = document.querySelector('#submitButton');
+// const email = document.querySelector('#email');
+// const password = document.querySelector('#password');
+// let loader = document.querySelector('.loader');
+// let Ending = document.querySelector('.ending');
+
 function isNotEmpty(value){
-    if (value == null || typeof value == 'undefined' || checked === true)
-    return false;
-    return(value.length > 0);
+  if (value !== '' || checked === true){
+      password.style.border = '2px solid';
+      password.style.borderRadius = '5px'
+      password.style.borderColor = '#dc3545';
+    }
+  else {
+      password.style.border = 'none';
+  
+    }
 }
 
 function validateEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
   }
 
-function SubmitLoad(){
-    
-}
+function removeSpinner() {
+  Ending.style.visibility = `visible`;
+  loader.style.display = 'none';
+  }
+
+// button.addEventListener('click', e => {
+//   isNotEmpty(value);
+//   emailValidation();   
+// });
+  
